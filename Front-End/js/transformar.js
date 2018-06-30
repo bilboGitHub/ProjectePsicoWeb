83e4e064cf0382bd4b476c422f12e1ef38ff629b
bilboGitHub <bilbobolsondefu@hotmail.com>
2018-06-30 19:47:59 +0200

    30_6_tarda

--- Front-End/js/transformar.js
+++ Front-End/js/transformar.js
@@ -15,6 +15,10 @@ $(document).ready(function() {
 // }
 // slide();
 
+
+//-------------------------------preus----------cards-------------------
+
+
 function slide() {
     $( ".trans_01" ).fadeToggle(2500, function(){
         $( ".trans_01" ).fadeToggle(2500, function(){
@@ -33,34 +37,6 @@ function slide() {
     });
 }
 slide();
-
-function flashcolor() {
-    $( ".flash_01" ).fadeToggle(1000, function(){
-        $( ".flash_01" ).fadeToggle(1000, function(){
-            $( ".flash_02" ).fadeToggle(1000, function(){
-                $( ".flash_02" ).fadeToggle(1000, function(){
-                    $( ".flash_03" ).fadeToggle(1000, function(){
-                        $( ".flash_03" ).fadeToggle(1000, function(){
-                            $( ".flash_04" ).fadeToggle(1000, function(){
-                                $( ".flash_04" ).fadeToggle(1000, function(){
-                                    $( ".flash_05" ).fadeToggle(1000, function(){
-                                        $( ".flash_05" ).fadeToggle(1000, function(){
-                                            $( ".flash_06" ).fadeToggle(1000, function(){
-                                                $( ".flash_06" ).fadeToggle(1000, flashcolor);
-                                            })
-                                        })
-                                    })
-                                })
-
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    });
-}
-flashcolor();
 
 function slide2() {
     $( ".trans_01" ).fadeToggle(2500, function(){
@@ -109,6 +85,191 @@ $(".canv_dime_ima1").css({
 $(".canv_dime_ima2").css({
     "width": dimensio/4
 })
+
+
+//-------------------------------flash----------banner-------------------
+
+
+// $("figure").css('background', function () {
+//     return $(this).data('color')
+// });
+
+// $(".tbc-banner").css('background', function () {
+//     return $(this).data('color')
+// });
+
+
+// $(".tbc-banner").css('background', function () {
+//     return $(this).data('color')
+// });
+
+
+
+// function canviaColor () {
+//     $(".tbc-banner").css({
+//         "background-color": "green"
+//     });
+// }
+
+
+
+function canviaColor__() { //function name
+    var color = '#'; // hexadecimal starting symbol
+    var letters = ['f8ddc2','dfcda3','cfddbb','e8eff2','f6dbfc']; //Set your colors here
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('posts').style.background = color; // Setting the random color on your div element.
+}
+
+function canviaColor () {
+    $(".tbc-banner").css('background', function () {
+        if ('background' === '#f8ddc2'){
+
+        }
+    });
+}
+
+
+
+// function nombreAleatori () {
+//     var number =  Math.floor(Math.random() * (5 - 1)) + 1;
+//     return number;
+// }
+
+
+
+// function colorAleatori () {
+//     var color = "#" + Math.floor(Math.random()*16777215).toString(16);
+//     return color;
+// }
+
+// console.log(colorAleatori());
+
+// function changeColor (){
+//     document.getElementById("color1").style.backgroundColor = colorAleatori ();
+// };
+
+
+
+function canviaColor() { 
+    var color = '#';
+    var letters = ['f8ddc2','dfcda3','cfddbb','e8eff2','f6dbfc','ffdce7','facad8','ffe6ca','e0dcd5'];
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('color1').style.background = color; 
+}
+
+
+function canviaMissatge () {
+    var paraula = document.getElementById("missatge1");
+    if (paraula.innerHTML == "S'HA TRENCAT LA CONFIANÇA"){
+        paraula.innerHTML = "PERDUA DE PES SOBTADA";
+    }
+    else if (paraula.innerHTML == "PERDUA DE PES SOBTADA") {
+        paraula.innerHTML = "PROBLEMES DE CONCENTRACIO, ATENCIO, MEMORIA";
+    }
+    else if (paraula.innerHTML == "PROBLEMES DE CONCENTRACIO, ATENCIO, MEMORIA") {
+        paraula.innerHTML = "COMPORTAMENT AGRESSIU, DESTRUCTIU O ENGANYOS";
+    }
+    else if (paraula.innerHTML == "COMPORTAMENT AGRESSIU, DESTRUCTIU O ENGANYOS") {
+        paraula.innerHTML = "SI ESTAS PREOCUPAT, NO ESPEREU";
+    }
+    else if (paraula.innerHTML == "SI ESTAS PREOCUPAT, NO ESPEREU") {
+        paraula.innerHTML = "S'HA TRENCAT LA CONFIANÇA";
+    }
+}
+function canviaTractament () {
+    var paraula = document.getElementById("tractament1");
+    if (paraula.innerHTML == "Teràpia de Parella"){
+        paraula.innerHTML = "Trastorns Alimentaris";
+    }
+    else if (paraula.innerHTML == "Trastorns Alimentaris") {
+        paraula.innerHTML = "Trastorn del Son";
+    }
+    else if (paraula.innerHTML == "Trastorn del Son") {
+        paraula.innerHTML = "Trastorn de Conducta";
+    }
+    else if (paraula.innerHTML == "Trastorn de Conducta") {
+        paraula.innerHTML = "Trastorns de l’Aprenentatge";
+    }
+    else if (paraula.innerHTML == "Trastorns de l’Aprenentatge") {
+        paraula.innerHTML = "Teràpia de Parella";
+    }
+}
+
+function canviaDireccio () {
+    var direccio = document.getElementById("direccio1");
+    if (direccio.href == "Front-End/03_parelles.html"){
+        direccio.href = "Front-End/03_adolescents.html";
+    }
+    else if (direccio.href == "Front-End/03_adolescents.html") {
+        direccio.href = "Front-End/03_adults.html";
+    }
+    else if (direccio.href == "Front-End/03_adults.html") {
+        direccio.href = "Front-End/03_adolescents.html";
+    }
+    else if (direccio.href == "Front-End/03_adolescents.html") {
+        direccio.href = "Front-End/03_infantil.html";
+    }
+    else if (direccio.href == "Front-End/03_infantil.html") {
+        direccio.href = "Front-End/03_parelles.html";
+    }
+}
+
+
+
+function canviaTot () {
+    canviaMissatge();
+    canviaTractament();
+    canviaDireccio();
+    canviaColor();
+}
+setInterval(canviaTot, 4000);
+
+// function flashcolor() {
+//     $( ".flash_01" ).fadeToggle(1000, function(){
+//         $( ".flash_01" ).fadeToggle(1000, function(){
+//             $( ".flash_02" ).fadeToggle(1000, function(){
+//                 $( ".flash_02" ).fadeToggle(1000, function(){
+//                     $( ".flash_03" ).fadeToggle(1000, function(){
+//                         $( ".flash_03" ).fadeToggle(1000, flashcolor);
+//                     })
+//                 })
+//             })
+//         })
+//     });
+// }
+// flashcolor();
+
+// function flashcolor() {
+//     $( ".flash_01" ).fadeToggle(1000, function(){
+//         $( ".flash_01" ).fadeToggle(1000, function(){
+//             $( ".flash_02" ).fadeToggle(1000, function(){
+//                 $( ".flash_02" ).fadeToggle(1000, function(){
+//                     $( ".flash_03" ).fadeToggle(1000, function(){
+//                         $( ".flash_03" ).fadeToggle(1000, function(){
+//                             $( ".flash_04" ).fadeToggle(1000, function(){
+//                                 $( ".flash_04" ).fadeToggle(1000, function(){
+//                                     $( ".flash_05" ).fadeToggle(1000, function(){
+//                                         $( ".flash_05" ).fadeToggle(1000, function(){
+//                                             $( ".flash_06" ).fadeToggle(1000, function(){
+//                                                 $( ".flash_06" ).fadeToggle(1000, flashcolor);
+//                                             })
+//                                         })
+//                                     })
+//                                 })
+
+//                             })
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     });
+// }
+// flashcolor();
+
+
+
+//-------------------------------flash----------hero-------------------
 
 function changing() {
     // Selecciono el div #a y le hago un toggleClass de la clase hide, con una duración de 1000ms, y que ejecutará una función al acabar, y así sucesivamente.
